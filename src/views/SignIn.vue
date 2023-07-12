10b99fc65f47bc6a44e5cbe16165698e55014ed5
Miroslav <mirautkin@gmail.com>
2023-07-12 14:46:47 +0300

    change gitignore

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,11 +1,45 @@
-<template>
-    <div>
-        SighnIn in my app
-    </div>
-</template>
-
 <script setup>
+import { ref } from 'vue';
+import { useAuthStore } from '../stores/auth';
+import { useRouter } from 'vue-router'
 
+import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
+import Message from 'primevue/message';
+import Loader from '../components/Loader.vue'
+
+const authStore = useAuthStore();
+const router = useRouter();
+
+const email = ref();
+const password = ref();
+
+const signin = async () => {
+    await authStore.auth({ email: email.value, password: password.value }, 'signin')
+    router.push('/cars')
+}
 </script>
 
-<style lang="css" scoped></style>
+<template>
+    <h2>Sign In</h2>
+    <form class="flex flex-column gap-3">
+        <Message v-if="authStore.error" severity="warn">{{ authStore.error }}</Message>
+        <div class="p-inputgroup flex-1">
+            <span class="p-inputgroup-addon">
+                <i class="pi pi-user"></i>
+            </span>
+            <InputText type="email" v-model="email" placeholder="Your Email" />
+        </div>
+        <div class="p-inputgroup flex-1">
+            <span class="p-inputgroup-addon">
+                <i class="pi pi-at"></i>
+            </span>
+            <InputText type="password" v-model="password" placeholder="Password" />
+        </div>
+        <Loader v-if="authStore.loader" />
+        <div v-else class="flex flex-column gap-3">
+            <Button label="Sign in" @click="signin" />
+            <span>Are you not registered yet? <router-link to="/signup">Sign in</router-link></span>
+        </div>
+    </form>
+</template>
