5d32750602a129717aedaea06076f6abe5d8693d
Miroslav <mirautkin@gmail.com>
2023-07-12 10:43:24 +0300

    delete assets

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -23,35 +23,41 @@ export const useAuthStore = defineStore('auth', () => {
         ...payload,
         returnSecureToken: true
       });
+      // let response = await axiosApiInstance.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=${apiKey}
+      // `, {
+      //   ...payload,
+      //   returnSecureToken: true
+      // });
       userInfo.value = {
         token: response.data.idToken,
         email: response.data.email,
         userId: response.data.localId,
         refreshToken: response.data.refreshToken,
       }
-      localStorage.setItem('userTokens', JSON.stringify({
-        token: userInfo.value.token,
-        refreshToken: userInfo.value.refreshToken
-      }))
+      // localStorage.setItem('userTokens', JSON.stringify({
+      //   token: userInfo.value.token,
+      //   refreshToken: userInfo.value.refreshToken
+      // }))
     } catch (err) {
-      switch (err.response.data.error.message) {
-        case 'EMAIL_EXISTS':
-          error.value = 'Email exists'
-          break;
-        case 'OPERATION_NOT_ALLOWED':
-          error.value = 'Operation not allowed'
-          break;
-        case 'EMAIL_NOT_FOUND':
-          error.value = 'Email not found'
-          break;
-        case 'INVALID_PASSWORD':
-          error.value = 'Invalid password'
-          break;
-        default:
-          error.value = 'Error'
-          break;
-      }
-      throw error.value;
+      console.log(err);
+      // switch (err.response.data.error.message) {
+      //   case 'EMAIL_EXISTS':
+      //     error.value = 'Email exists'
+      //     break;
+      //   case 'OPERATION_NOT_ALLOWED':
+      //     error.value = 'Operation not allowed'
+      //     break;
+      //   case 'EMAIL_NOT_FOUND':
+      //     error.value = 'Email not found'
+      //     break;
+      //   case 'INVALID_PASSWORD':
+      //     error.value = 'Invalid password'
+      //     break;
+      //   default:
+      //     error.value = 'Error'
+      //     break;
+      // }
+      // throw error.value;
     } finally {
       loader.value = false;
     }
