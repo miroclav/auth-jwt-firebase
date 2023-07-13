dce2313eeedd9039fd254e242ed2f662e6cf4a73
Miroslav <mirautkin@gmail.com>
2023-07-13 16:22:08 +0300

    add users.vue

---
+++ src/views/Users.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>Cars</h2>
+        <Loader v-if="showLoader" />
+        <div class="flex flex-column gap-3" v-else>
+            <Card v-for="(car, i) in cars" :key="i">
+                <template #title> {{ car.name }} </template>
+                <template #subtitle> {{ car.type }} </template>
+            </Card>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="css" scoped></style>
